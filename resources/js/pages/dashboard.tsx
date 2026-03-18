a3393c41a51fae883fcf171e0b426544a480135c
vincet0t15 <vincet0t15@gmail.com>
2026-03-18 16:31:03 +0800

    asdf

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -2,7 +2,7 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { NavMain } from '../components/NavMain2';
+import { Button } from '@/components/ui/button';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -18,6 +18,7 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
 
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <Button variant="default">asdf</Button>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
