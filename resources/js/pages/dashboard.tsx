cf6140818ee2dcb4cd00e1fbc7e684b6ea9cb2b2
vincet0t15 <vincet0t15@gmail.com>
2026-03-18 16:47:56 +0800

    asdf

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -16,9 +16,7 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <Button variant="default">asdf</Button>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
